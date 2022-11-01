66f6a991f49a46c81bb279f02e03bc8c1f5a94b2
Amanuel Negussie <Anegussie9077@conestogac.on.ca>
2022-11-01 15:18:33 -0400

    This is a commit that has an updated build.gradle. Local tests work and will check if circle ci can work off the gradle tasks provided

---
+++ src/test/java/ScreenTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScreenTest {
+
+    @Test
+    void displayMessage() {
+
+    }
+
+    @Test
+    void displayMessageLine() {
+    }
+
+    @Test
+    void displayDollarAmount() {
+    }
+
+    @Test
+    void getMessage() {
+    }
+
+    @Test
+    void getDollarAmount() {
+    }
+}
---
+++ src/test/java/TransactionTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionTest {
+
+    @Test
+    void getAccountNumber() {
+    }
+
+    @Test
+    void getScreen() {
+    }
+
+    @Test
+    void getBankDatabase() {
+    }
+
+    @Test
+    void execute() {
+    }
+}
